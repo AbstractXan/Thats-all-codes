d06cf4ee1fe95bdb8ccec4465b88cf1310e6a25e
Priyansh Sangule <priyansh0506@gmail.com>
2018-04-04 23:46:17 +0530

    Update main.py

--- GithubAPI/main.py
+++ GithubAPI/main.py
@@ -1,13 +1,16 @@
 import requests as req
 import json
 import time
+import base64
+import os
+
+##curr_dir = os.getcwd();
+##print curr_dir
 
 rawrequest =req.get('https://api.github.com/search/repositories?q=language:java&sort=stars')
 datajson = rawrequest.json()
 
-#Writes code files
-with open('file.txt', 'w') as file:
-     file.write(str(datajson))
+
 
 #
 temp1 = datajson['items']
@@ -22,9 +25,20 @@ for i in temp1:
 
 #Acceses code in repository
 for rep_name in repos:       
-    try:
+##    try:
+##
+##        #Change directory to add codes for each repositories
+##        os.mkdir(str(rep_name))
+##        os.chdir(curr_dir+'/'+str(rep_name))
+##        print os.getcwd()
+##    except Exception as ex:
+##        print(str(ex))
+##        break
+    
+    try:    
         print " "
         print rep_name
+        
         #Create new urls based on repo names fetched
         repos_url='https://api.github.com/search/code?q=in:file+language:java+repo:'+str(rep_name)
         repos_request = req.get(repos_url)
@@ -36,29 +50,29 @@ for rep_name in repos:
         print(repos_request.headers['X-RateLimit-Remaining'])
         repos_request_json = repos_request.json()    
 
+       
         #Get new git_urls for source
         code_url_list = repos_request_json['items']
-
         for j in code_url_list:
-            print(j['git_url'])
+            code_url=(j['git_url'])
+            code_req=req.get(code_url)
+            code_request_json=code_req.json()
+            code=base64.b64decode(code_request_json['content'])
+            
+            code_name=j['name']
+            
+            #Writes code files
+            with open(code_name, 'w') as file:
+                file.write(code)
         
+            
     except Exception as e:
         print(str(e))
         print (repos_request_json['message'])
-
-##        srch_wait_req=( req.get('https://api.github.com/rate_limit') ).json()
-##        
-##        wait_time = float(srch_wait_req['resources']['search']['reset'])
-##
-##        print (' Resuming time:' ,wait_time)
-##        print (' Current time:', time.time())
-##        wait_time = wait_time - time.time() -19800 ######GMT lag wrt location
-##        print wait_time
-##        if (wait_time > 0):
-##            print ('Waiting for ', wait_time/(60) ,' minutes...')
-##            time.wait(wait_time)
-##            print ('Resuming...')
-##        
-##        #time.sleep(5)
         continue
     
+    
+
+
+
+
